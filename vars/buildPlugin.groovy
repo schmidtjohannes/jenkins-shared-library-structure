1f0abe07b675141e6209ea5cc5070c0f7179053e
Johannes Schmidt <johannes.schmidt.delaunay@gmail.com>
2020-03-11 16:37:25 -0300

    Update buildPlugin.groovy

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,7 +1,5 @@
 def call(Map config) {
     node {
-        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
-        sh 'mvn install'
-        mail to: '...', subject: "${config.name} plugin build", body: '...'
+        sh 'echo Hi'
     }
 }

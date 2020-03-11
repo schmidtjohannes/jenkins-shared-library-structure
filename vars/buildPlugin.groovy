742382937606c89f8fa10d0c854fa915d5138360
Johannes Schmidt <johannes.schmidt.delaunay@gmail.com>
2020-03-11 16:57:43 -0300

    Update buildPlugin.groovy

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,6 +1,8 @@
 def call(Map config) {
     node {
-        agent devops-jenkins-agent
+        agent {
+            label 'devops-jenkins-agent'
+        }
         stage "Say Hi"
         sh "echo Hi ${config.url}"
         stage "Say Hi Again"
